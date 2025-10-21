6b0cd79101a8dcd9d3866b9c76cd4f1ed43967c2
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-10-20 21:14:42 -0300

    Welcome page Java code

--- app/src/main/java/com/aghakhani/welcomeapp/WelcomeActivity.java
+++ app/src/main/java/com/aghakhani/welcomeapp/WelcomeActivity.java
@@ -2,23 +2,39 @@ package com.aghakhani.welcomeapp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private MaterialButton continueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Set status bar color
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.status_bar));
+
+        // Setup continue button
+        continueBtn = findViewById(R.id.continueBtn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, SignInActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
         });
     }
 }
